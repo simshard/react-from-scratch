7636e8b1c88cacb5846f050402abfe709ea890dd
Sim <simshard@gmail.com>
2025-04-18 04:26:16 +0100

    tweak App

--- src/entry.jsx
+++ src/entry.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./app.css";
-import { App } from "./app.jsx";
+import { App } from "./App.jsx";
 
 createRoot(document.getElementById("app")).render(
   <StrictMode>
