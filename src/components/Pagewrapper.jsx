6085f7ed977547a3f9cb6821ac33a84f2bc3982d
Sim <simshard@gmail.com>
2025-04-29 00:27:36 +0100

    puppylist

--- src/components/Pagewrapper.jsx
+++ src/components/Pagewrapper.jsx
@@ -1,6 +1,6 @@
 export function  Pagewrapper({children}) {
     return (
-      <div className="min-h-dvh bg-gradient-to-b from-orange-200 to-purple-400/100 to-[60vh]">
+      <div className="min-h-dvh bg-gradient-to-b from-orange-200 to-white to-[60vh]">
         {children}
       </div>
     );
