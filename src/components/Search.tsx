3dc3681ec3b50ca04855ba71840889df0878b805
Sim <simshard@gmail.com>
2025-05-16 02:30:42 +0100

    ref!

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -16,6 +16,7 @@ export function Search({
     </label>
     <div className="mt-2 flex items-center gap-4">
       <input
+      ref ={inputRef}
       value={searchQuery}
       onChange={(e) => setSearchQuery(e.target.value)}
         placeholder="filter..."
@@ -24,7 +25,12 @@ export function Search({
         type="text"
         className="w-full max-w-80 bg-white px-4 py-2 ring ring-black/5 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
       />
-      <button className="inline-block rounded bg-cyan-300 px-4 py-2 !pr-3 !pl-2.5 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none">
+      <button 
+      onClick={() => {
+            setSearchQuery("");
+            inputRef.current.focus();
+          }}
+      className="inline-block rounded bg-indigo-300 px-4 py-2 !pr-3 !pl-2.5 font-medium text-cyan-900 hover:bg-indigo-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width={24}
